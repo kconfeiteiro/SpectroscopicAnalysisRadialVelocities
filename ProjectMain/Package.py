4ceec8421b3542f49689c0245993f4cf0e186d29
Krystian Confeiteiro <confeitk@my.erau.edu>
2023-04-21 11:31:14 -0400

    clead up the files

---
+++ ProjectMain/Package.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.wcs import WCS
+import numpy as np
+import os
+
+'''
+Links (sources)
+-----
+1. https://astronomy.stackexchange.com/questions/43552/working-with-stellar-spectra-in-fits-format-in-python
+
+'''
+
+class AnalyzeSpectra:
+
+    def __init__(self, path):
+        self.path = path
+        self.directory = os.listdir(self.path)
+
+        if '.fit' in self.path: 
+            self.filename = self.path.split('/')[-1].replace('.fit','')
+            self.target = self.filename.split('_')[0]
+            self.title = self.target + ', Number: ' + self.filename.split('_')[-1]
+        else:
+            self.target = self.directory[0].split('/')[-1].replace('.fit','').split('_')[0]
+            self.title = 'Partial Spectra of ' + self.target
+
+    def read_signal(self, file=None):
+        if file is None: file = self.path
+        sp = fits.open(file)
+        header = sp[0].header
+
+        wcs = WCS(header)
+        index = np.arange(header['NAXIS1'])
+
+        wavelength = wcs.wcs_pix2world(index[:,np.newaxis], 0)
+        self.wavelength = wavelength.flatten()
+        self.flux = sp[0].data
+
+        return self.wavelength, self.flux
+    
+    def smooth_signal(self, y):
+        return np.linalg.svd(y, full_matrices=False)
+    
+    def flatten_list(self, x):
+        return [item for subl in x for item in subl]
+    
+    def create_spectra(self, save=False):
+        self.x, self.y = [0]*len(self.directory), [0]*len(self.directory)
+        for i, file in enumerate(self.directory):
+            file = os.path.join(self.path, file)
+            x1, y1 = self.read_signal(file)
+            self.x[i] = x1
+            self.y[i] = y1
+
+        self.x, self.y = self.flatten_list(self.x), self.flatten_list(self.y)
+        _, self.y, _ = self.smooth_signal([self.y])
+
+
+        if save:
+            return self.x, self.y
+        
+    def plot_spectra(self, X=None, y=None, title=None, x_title='Wavelength (Angstroms)', y_title=' Normalized Flux', file='PLOTS/', print=False, save=False):
+        if title is None: title = self.title
+        if X is not None: X, y = self.wavelength, self.flux
+
+        plt.plot(X, y)
+        plt.xlabel(x_title)
+        plt.ylabel(y_title)
+        plt.title(title)
+        plt.tight_layout()
+        if save: plt.savefig(file + self.target + '_full_spectra.jpg')
+        if print: plt.show()
+
+    def plot_full_spectra(self, **kwargs):
+        x, y = self.create_spectra(save=True)
+        self.plot_spectra(x, y, title='Full Spectra of ' + self.target, **kwargs)
+
+
+class CalculateParamaters(AnalyzeSpectra):
+
+    def __init__(self, parameters:dict):
+        self.parameters = parameters
--- README.md
+++ README.md
@@ -1,16 +1,50 @@
 # EP 425 Final Project Code
 
-### Project Description
+## How to to use this package
 
-#### Purpose
+### Import
+
+Importing this package is pretty simple. Follow these steps:
+
+1. Import entire code using `pip install git+https://github.com/[kconfeiteiro]/[EP-425-Final-Project-Code]@[main]`
+2. Now, you need to import the functions to read, plot, and make the calculations. For that, use `from ProjectMain.Package import AnalyzeSpectra`
+
+#### Example
+
+##### Code
+
+```python
+from ProjectMain import Package as spec
+import os
+
+# define root folder and indvidual files you want to plot (separetely)
+file = 'HD12871704_15_23_850_20230415064551_47.fit'
+root = 'DATA/HD12871704_15_23_850_-1_20230415T064551/'
+path = os.path.join(root, file) # creates a combined path
+
+# initialie class and perform operations
+Data = spec.AnalyzeSpectra(root) # initialize class
+Data.plot_full_spectra(print=True) # plots entire combined spectra
+```
+
+##### Code Outline
+
+1. `spec.AnalyzeSpectra(root, file)` initializes the class (where the functions are)
+2. `root` is the file path that leads to your files
+3. `file` is a single file that you can plot instead of the entire combined spectra
+4. `Data.plot_full_spectra()` is where you plot the entire spectra, print=True tells the code to show the plot
+
+## Project Description
+
+### Purpose
 
 This code is for the final project for EP 425, Observational Astronomy, for Spring 2023.
 
-#### Project Objective
+### Project Objective
 
 Our main objective is to fit a periodic curve to shifting absorption lines (like Hydrogen and Calcium lines) of the system’s spectra to confirm the presence of exoplanets. We are looking at the star system *HD 128717* from theGAIA DR3 Archival Database. Our target system was observed with Gaia as an exoplanet candidate using photometry.
 
-### Project Team
+### Team
 
 * Johnathen Saier
 * Krystian Confeiteiro
@@ -30,9 +64,14 @@ Our main objective is to fit a periodic curve to shifting absorption lines (like
 2. Harris, C.R., Millman, K.J., van der Walt, S.J. et al.  *Array programming with NumPy* . Nature 585, 357–362 (2020). DOI: [10.1038/s41586-020-2649-2](https://doi.org/10.1038/s41586-020-2649-2). ([Publisher link](https://www.nature.com/articles/s41586-020-2649-2)).
 3. J. D. Hunter, "Matplotlib: A 2D Graphics Environment", Computing in Science & Engineering, vol. 9, no. 3, pp. 90-95, 2007 ([Publisher link](https://doi.org/10.1109/MCSE.2007.55)).
 
-### Code Contributers
+#### Code Contributers
 
 * Krystian Confeiteiro
 
-### Observations 
+#### Observations
+
 * All completed by John Saier
+
+#### Data Reduction and Calibration
+
+* Completed by John Saier using Demetra
