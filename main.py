6b69a08dd7fcf93de71af85ea82db23718188848
kconfeiteiro <confeitk@outlook.com>
2023-09-17 18:42:44 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from ProjectMain.Package import (
-	AnalyzeSpectra, ImportSpectra, 
+	AnalyzeSpectra, ImportSpectra,
 	PlotSpectra, CompareSpectra
 )
 from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
 import os
 
+
+[print("Hello world") for _ in range(100)]
+
+exit()
 if __name__ == '__main__':
 
 	file = 'HD12871704_15_23_850_20230415064551_34.fit'
@@ -14,7 +18,7 @@ if __name__ == '__main__':
 
 	MAIN = PlotSpectra(path, order=37, filetype='.fit', target='HD 128717', smooth_all=True)
 	# MAIN.plot_spectra(x_title='Wavelength (Angstroms)', y_title='Flux', tight_layout=True, show_lowest=3)
-	
+
 	ANT_root = 'DATA/Antonio Spectra/HD 46150 (4.7.23) Full Spectra'
 	file1 = 'DATA/Antonio Spectra/HD 46150 (4.7.23) Full Spectra/HD 46150 (4.7.23)_20230408012534_46.fit'
 	PTH1 = 'DATA/Antonio Spectra/HD 46150 (4.7.23) Full Spectra/'
@@ -47,7 +51,7 @@ if __name__ == '__main__':
 	# plt.legend(['04/07','04/20','03/21 (diff. Target)'])
 
 	# FOLDERS = [
-		# 'DATA/Antonio Spectra/HD 46150 (4.7.23) Full Spectra/', 
+		# 'DATA/Antonio Spectra/HD 46150 (4.7.23) Full Spectra/',
 		# 'DATA/Antonio Spectra/HD 46150 (4.20.23)_-1_20230421T012334',
 		# 'DATA/Antonio Spectra/HD 46150 (old-bad) Full Spectra'
 	# ]
@@ -57,4 +61,3 @@ if __name__ == '__main__':
 	# print(files)
 	# needed = COMP.find_orders(order=43, filetype='.fit')
 	# COMP.plot_comparison(x_label='Wavelength', y_label='Flux')
-	
